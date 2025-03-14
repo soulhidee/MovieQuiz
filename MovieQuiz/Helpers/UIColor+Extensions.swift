281da959e84f02240ee4d3eb242194f8e6b1264a
Daniil Isakov <serverpgig@yandex.ru>
2025-03-14 18:02:25 +0300

    add all elements to main screen and set constraints

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,10 +1,4 @@
 import UIKit
 
 extension UIColor {
-     static var ypGreen: UIColor { UIColor(named: "assetYPGreen") ?? UIColor.green }
-     static var ypRed: UIColor { UIColor(named: "assetYPRed") ?? UIColor.red }
-     static var ypBlack: UIColor { UIColor(named: "assetYPBlack") ?? UIColor.black }
-     static var ypBackground: UIColor { UIColor(named: "assetYPBackground") ?? UIColor.darkGray }
-     static var ypGray: UIColor { UIColor(named: "assetYPGray") ?? UIColor.gray }
-     static var ypWhite: UIColor { UIColor(named: "assetYPWhite") ?? UIColor.white }
 }
