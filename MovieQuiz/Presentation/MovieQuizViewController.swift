488775f3aede152f591e83319adfd4db675f40dc
Daniil Isakov <serverpgig@yandex.ru>
2025-03-15 19:33:08 +0300

    display quiz results alert

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,6 +7,7 @@ final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+//        showNextQuestionOrResults()
     }
     
     @IBAction private func noButtonClicked(_ sender: Any) {
@@ -28,13 +29,18 @@ final class MovieQuizViewController: UIViewController {
     }
     
     struct QuizStepViewModel {
-      // картинка с афишей фильма с типом UIImage
       let image: UIImage
-      // вопрос о рейтинге квиза
       let question: String
-      // строка с порядковым номером этого вопроса (ex. "1/10")
       let questionNumber: String
     }
+    
+    struct QuizResultsViewModel {
+      let title: String
+      let text: String
+      let buttonText: String
+    
+    }
+    
     
     private let questions: [QuizQuestion] = [
         QuizQuestion(
@@ -87,10 +93,11 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
+    
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
-
-    private func convent(model: QuizQuestion) -> QuizStepViewModel {
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel (
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
@@ -105,6 +112,10 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
@@ -115,15 +126,50 @@ final class MovieQuizViewController: UIViewController {
         }
     }
     
-    private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questions.count - 1 {
-            
-        } else {
-            currentQuestionIndex += 1
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.imageView.layer.masksToBounds = false
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
         }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
-}
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10" // 1
+            let viewModel = QuizResultsViewModel( // 2
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel) // 3
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    }
+    
+    
+    
+    
+
+    
+    
 
 /*
  Mock-данные
