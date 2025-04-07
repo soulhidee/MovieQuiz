cb989e35589917005d22cf07b0dd78627691b58c
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 21:03:23 +0300

    feat: add gamesCount property using UserDefaults

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -1,1 +1,22 @@
 import Foundation
+
+class StatisticService: StatisticServiceProtocol {
+    var gamesCount: Int {
+        get {
+            UserDefaults.standard.integer(forKey: "gamesCount")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "gamesCount")
+        }
+    }
+    
+    var bestGame: GameResult
+    
+    var totalAccuracy: Double
+    
+    func store(correct count: Int, total amount: Int) {
+        
+    }
+    
+    
+}
