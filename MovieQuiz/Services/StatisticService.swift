d86689122d29387c3e3b8133a8b9aa199140b023
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 21:12:54 +0300

    feat: implement bestGame property using UserDefaults

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -10,9 +10,22 @@ class StatisticService: StatisticServiceProtocol {
         }
     }
     
-    var bestGame: GameResult
+    var bestGame: GameResult {
+        get {
+            let correct = UserDefaults.standard.integer(forKey: "bestGameCorrect")
+            let total = UserDefaults.standard.integer(forKey: "bestGameTotal")
+            let date = UserDefaults.standard.object(forKey: "bestGameDate") as? Date ?? Date()
+            
+            return GameResult(correct: correct, total: total, date: date)
+        }
+        set {
+            UserDefaults.standard.set(newValue.correct, forKey: "bestGameCorrect")
+            UserDefaults.standard.set(newValue.total, forKey: "bestGameTotal")
+            UserDefaults.standard.set(newValue.date, forKey: "bestGameDate")
+        }
+    }
     
-    var totalAccuracy: Double
+    var totalAccuracy: Double = 0.0
     
     func store(correct count: Int, total amount: Int) {
         
