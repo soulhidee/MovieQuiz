0d4babb340af2df1623f0a70445fcb6aa1c8c6fd
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 21:26:47 +0300

    replace string keys with enum Keys for UserDefaults

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -1,34 +1,45 @@
 import Foundation
 
 final class StatisticService: StatisticServiceProtocol {
+    private enum Keys: String {
+        case gamesCount
+        case bestGameCorrect
+        case bestGameTotal
+        case bestGameDate
+        case totalCorrect
+        case totalQuestions
+    }
+    
     private let storage: UserDefaults = .standard
+    
+    
     var gamesCount: Int {
         get {
-            storage.integer(forKey: "gamesCount")
+            storage.integer(forKey: Keys.gamesCount.rawValue)
         }
         set {
-            storage.set(newValue, forKey: "gamesCount")
+            storage.set(newValue, forKey: Keys.gamesCount.rawValue)
         }
     }
     
     var bestGame: GameResult {
         get {
-            let correct = storage.integer(forKey: "bestGameCorrect")
-            let total = storage.integer(forKey: "bestGameTotal")
-            let date = storage.object(forKey: "bestGameDate") as? Date ?? Date()
+            let correct = storage.integer(forKey: Keys.bestGameCorrect.rawValue)
+            let total = storage.integer(forKey: Keys.bestGameTotal.rawValue)
+            let date = storage.object(forKey: Keys.bestGameDate.rawValue) as? Date ?? Date()
             
             return GameResult(correct: correct, total: total, date: date)
         }
         set {
-            storage.set(newValue.correct, forKey: "bestGameCorrect")
-            storage.set(newValue.total, forKey: "bestGameTotal")
-            storage.set(newValue.date, forKey: "bestGameDate")
+            storage.set(newValue.correct, forKey: Keys.bestGameCorrect.rawValue)
+            storage.set(newValue.total, forKey: Keys.bestGameTotal.rawValue)
+            storage.set(newValue.date, forKey: Keys.bestGameDate.rawValue)
         }
     }
     
     var totalAccuracy: Double {
-        let correctAnswers = storage.integer(forKey: "totalCorrect")
-        let totalQuestions = storage.integer(forKey: "totalQuestions")
+        let correctAnswers = storage.integer(forKey: Keys.totalCorrect.rawValue)
+        let totalQuestions = storage.integer(forKey: Keys.totalQuestions.rawValue)
         
         guard totalQuestions != 0 else {
             return 0.0
