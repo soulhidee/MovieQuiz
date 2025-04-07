b192bf5e5617ff9e4a2d83347e2ec357fb782d5b
Daniil Isakov <serverpgig@yandex.ru>
2025-04-07 20:57:29 +0300

    add file StatisticService

--- MovieQuiz/Models/GameResult.swift
+++ MovieQuiz/Models/GameResult.swift
@@ -4,4 +4,9 @@ struct GameResult {
     let correct: Int
     let total: Int
     let date: Date
+    
+    func isBetterThan(_ another: GameResult) -> Bool {
+        correct > another.correct
+    }
 }
+
