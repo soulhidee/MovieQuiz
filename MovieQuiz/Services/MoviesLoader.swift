bd2761a887e9aae98213b5f5b30f16c5be3d84fc
Daniil Isakov <serverpgig@yandex.ru>
2025-04-19 13:44:05 +0300

    implement loadMovies method in MoviesLoader

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -1,8 +1,33 @@
-//
-//  MoviesLoader.swift
-//  MovieQuiz
-//
-//  Created by Даниил on 19.04.2025.
-//
-
 import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, any Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+               case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let mostPopularMovies = try decoder.decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+    
+}
