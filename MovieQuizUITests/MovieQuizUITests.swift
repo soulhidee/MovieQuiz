563fde4f29b8c1cd83852b3158e7816d305fd148
Daniil Isakov <serverpgig@yandex.ru>
2025-04-24 21:20:57 +0300

    feat: Write correct test for alert appearance after round completion

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -37,12 +37,45 @@ class MovieQuizUITests: XCTestCase {
         
         XCTAssertNotEqual(firstPosterData, secondPosterData)
     }
-
+    
     func testYesButton() {
         testButtonAction(buttonTitle: "Yes", expectedIndex: "2/10")
     }
-
+    
     func testNoButton() {
         testButtonAction(buttonTitle: "No", expectedIndex: "2/10")
     }
+    
+    func testAlertAppearsAfterLastQuestion() {
+        let button = app.buttons["Yes"]
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertTrue(button.waitForExistence(timeout: 5))
+        XCTAssertTrue(indexLabel.waitForExistence(timeout: 5))
+        
+        for i in 1...10 {
+            button.tap()
+            let expectedLabel: String
+            
+            if i == 10 {
+                expectedLabel = "10/10"
+            } else {
+                expectedLabel = "\(i + 1)/10"
+            }
+            
+            let predicate = NSPredicate(format: "label == %@", expectedLabel)
+            let expectation = XCTNSPredicateExpectation(predicate: predicate, object: indexLabel)
+            let result = XCTWaiter().wait(for: [expectation], timeout: 5)
+            XCTAssertEqual(result, .completed, "Ожидали индекс: \(expectedLabel), получили \(indexLabel.label)")
+        }
+        
+        let alert = app.alerts["Этот раунд окончен!"]
+        XCTAssertTrue(alert.waitForExistence(timeout: 5))
+        app.buttons["Сыграть ещё раз"].tap()
+        
+        XCTAssertTrue(indexLabel.waitForExistence(timeout: 5))
+        XCTAssertEqual(indexLabel.label, "1/10")
+    }
+    
+
 }
