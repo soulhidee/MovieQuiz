44ba1842d16f73c29a8988787c85d6bd096a5a51
Daniil Isakov <serverpgig@yandex.ru>
2025-04-24 13:57:01 +0300

    add unit tests for MoviesLoader

---
+++ MovieQuizTest/ArrayTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueToRange() throws {
+        //Given
+        let array = [1, 1, 2, 3, 5]
+        
+        //When
+        let value = array[safe: 2]
+        
+        //Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+
+    func testGetValueOutRange() throws {
+        //Given
+        let array = [1, 1, 2, 3, 5]
+        
+        //When
+        let value = array[safe: 20]
+        
+        //Then
+        XCTAssertNil(value)
+    }
+    
+}
