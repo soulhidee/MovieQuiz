415bc59ececb3fcee593bd836e6b072f592ba038
Daniil Isakov <serverpgig@yandex.ru>
2025-04-24 15:37:10 +0300

    UITest

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -1,41 +1,44 @@
-//
-//  MovieQuizUITests.swift
-//  MovieQuizUITests
-//
-//  Created by Даниил on 24.04.2025.
-//
 
 import XCTest
 
-final class MovieQuizUITests: XCTestCase {
-
+class MovieQuizUITests: XCTestCase {
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        continueAfterFailure = false
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testScreenCast() throws {
+        let app = XCUIApplication()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Нет"]/*[[".buttons[\"Нет\"].staticTexts.firstMatch",".buttons.staticTexts[\"Нет\"]",".staticTexts[\"Нет\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let staticText = app/*@START_MENU_TOKEN@*/.staticTexts["Да"]/*[[".buttons[\"Да\"].staticTexts.firstMatch",".buttons.staticTexts[\"Да\"]",".staticTexts[\"Да\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        staticText.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Нет"]/*[[".buttons.containing(.staticText, identifier: \"Нет\").firstMatch",".otherElements.buttons[\"Нет\"]",".buttons[\"Нет\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        staticText.tap()
+        
+        let element = app.otherElements/*@START_MENU_TOKEN@*/.containing(.button, identifier: "Нет").firstMatch/*[[".element(boundBy: 5)",".containing(.staticText, identifier: \"Нет\").firstMatch",".containing(.button, identifier: \"Да\").firstMatch",".containing(.button, identifier: \"Нет\").firstMatch"],[[[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        element.tap()
+        staticText.tap()
+        element.tap()
+        staticText.tap()
+        staticText.tap()
+        staticText.tap()
+        staticText.tap()
+        staticText.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["Сыграть ещё раз"]/*[[".otherElements.buttons[\"Сыграть ещё раз\"]",".buttons.firstMatch",".buttons[\"Сыграть ещё раз\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+                
     }
 }
