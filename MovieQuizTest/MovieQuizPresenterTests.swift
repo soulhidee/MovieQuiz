2c4202ac3890756bd92b6e9ddccef30233f6ba06
Daniil Isakov <serverpgig@yandex.ru>
2025-04-29 18:38:32 +0300

    test: add StubStatisticService and StubQuestionFactory

--- MovieQuizTest/MovieQuizPresenterTests.swift
+++ MovieQuizTest/MovieQuizPresenterTests.swift
@@ -28,3 +28,13 @@ final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
 }
 
 
+final class StubStatisticService: StatisticServiceProtocol {
+    var gamesCount: Int = 0
+    var bestGame = MovieQuiz.GameResult(correct: 0, total: 0, date: Date())
+    var totalAccuracy: Double = 0.0
+    
+    func store(correct count: Int, total amount: Int) {
+        
+    }
+}
+
